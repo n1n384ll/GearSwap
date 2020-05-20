5a18b139af4feca31fffc9ffca405f78d30b95f1
Kenta Sugimoto <sugimoto.kenta.9@gmail.com>
2020-05-20 19:47:06 -0400

    thf updates

--- data/Nineball_THF.lua
+++ data/Nineball_THF.lua
@@ -25,92 +25,161 @@ function get_sets()
     --]]
 
     sets.idle = {
-        --main="",
-        --sub="",
-        --range="",
-        ammo="Impatiens",
-        head="Vitiation chapeau",
-        neck="Twilight torque",
-        left_ear="Estoqueur's earring",
-        right_ear="Loquacious earring",
-        body="Jhakri Robe",
-        hands="Atrophy Gloves +1",
-        left_ring="Shneddick ring",
-        right_ring="Warp ring",
-        back="Shadow mantle",
-        waist="Fucho-no-obi",
+        ammo="Iron Gobbet",
+        head="Lithelimb Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
         legs="Dashing Subligar",
-        feet={ name="Gende. Galosh. +1", augments={'Phys. dmg. taken -3%','Magic dmg. taken -2%','Song recast delay -3',}},
+        feet="Skulk. Poulaines",
+        neck="Twilight Torque",
+        waist="Chaac Belt",
+        left_ear="Infused Earring",
+        right_ear="Colossus's Earring",
+        left_ring="Shneddick Ring",
+        right_ring={ name="Dark Ring", augments={'Magic dmg. taken -3%','Phys. dmg. taken -4%','Breath dmg. taken -3%',}},
+        back="Shadow Mantle",
     }
 
     sets.melee = {
-        ammo="",
-        head="",
-        neck="",
-        left_ear="",
-        right_ear="",
-        body="",
-        hands="",
-        left_ring="",
-        right_ring="",
-        back="",
-        waist="",
-        legs="",
-        feet=""
+        ammo="Hagneia Stone",
+        head="Lithelimb Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
+        legs="Ighwa Trousers",
+        feet="Skulk. Poulaines",
+        neck="Asperity Necklace",
+        waist="Chaac Belt",
+        left_ear="Dudgeon Earring",
+        right_ear="Heartseeker Earring",
+        left_ring="Epona's Ring",
+        right_ring="Rajas Ring",
+        back={ name="Canny Cape", augments={'DEX+2','AGI+3','"Dual Wield"+2','Crit. hit damage +1%',}},
     }
 
     sets.dt = {
-        ammo="",
-        head="",
-        neck="",
-        left_ear="",
-        right_ear="",
-        body="",
-        hands="",
-        left_ring="",
-        right_ring="",
-        back="",
-        waist="",
-        legs="",
-        feet=""
+        ammo="Demonry Stone",
+        head="Lithelimb Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
+        legs="Ighwa Trousers",
+        feet="Skulk. Poulaines",
+        neck="Twilight Torque",
+        waist="Chaac Belt",
+        left_ear="Infused Earring",
+        right_ear="Colossus's Earring",
+        left_ring={ name="Dark Ring", augments={'Magic dmg. taken -3%','Phys. dmg. taken -4%','Breath dmg. taken -3%',}},
+        right_ring={ name="Dark Ring", augments={'Enemy crit. hit rate -2','Spell interruption rate down -4%','Phys. dmg. taken -4%',}},
+        back="Shadow Mantle",
     }
 
     sets.sneak_attack = {
-        ammo="",
-        head="",
-        neck="",
-        left_ear="",
-        right_ear="",
-        body="",
-        hands="",
-        left_ring="",
-        right_ring="",
-        back="",
-        waist="",
-        legs="",
-        feet=""
+        ammo="Demonry Core",
+        head="Uk'uxkaj Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
+        legs="Manibozho Brais",
+        feet="Skulk. Poulaines",
+        neck="Asperity Necklace",
+        waist="Chaac Belt",
+        left_ear="Kuwunga Earring",
+        right_ear="Ethereal Earring",
+        left_ring="Apate Ring",
+        right_ring="Rajas Ring",
+        back={ name="Canny Cape", augments={'DEX+2','AGI+3','"Dual Wield"+2','Crit. hit damage +1%',}},
     }
 
     sets.trick_attack = {
-        ammo="",
-        head="",
-        neck="",
-        left_ear="",
-        right_ear="",
-        body="",
-        hands="",
-        left_ring="",
-        right_ring="",
-        back="",
-        waist="",
-        legs="",
-        feet=""
+        ammo="Qirmiz Tathlum",
+        head="Lithelimb Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
+        legs="Ighwa Trousers",
+        feet="Skulk. Poulaines",
+        neck="Houyi's Gorget",
+        waist="Chaac Belt",
+        left_ear="Infused Earring",
+        right_ear="Suppanomimi",
+        left_ring="Stormsoul Ring",
+        right_ring="Stormsoul Ring",
+        back={ name="Canny Cape", augments={'DEX+2','AGI+3','"Dual Wield"+2','Crit. hit damage +1%',}},
     }
 
+    sets.sata = {
+        ammo="Qirmiz Tathlum",
+        head="Lithelimb Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
+        legs="Ighwa Trousers",
+        feet="Skulk. Poulaines",
+        neck="Houyi's Gorget",
+        waist="Chaac Belt",
+        left_ear="Infused Earring",
+        right_ear="Kuwunga Earring",
+        left_ring="Apate Ring",
+        right_ring="Rajas Ring",
+        back={ name="Canny Cape", augments={'DEX+2','AGI+3','"Dual Wield"+2','Crit. hit damage +1%',}},
+    }
+
+    sets.alien_edge = {
+        ammo="Demonry Core",
+        head="Uk'uxkaj Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
+        legs="Ighwa Trousers",
+        feet="Skulk. Poulaines",
+        neck="Breeze Gorget",
+        waist="Chaac Belt",
+        left_ear="Ishvara Earring",
+        right_ear={ name="Moonshade Earring", augments={'Attack+4','TP Bonus +250',}},
+        left_ring="Acumen Ring",
+        right_ring="Thundersoul Ring",
+        back={ name="Canny Cape", augments={'DEX+2','AGI+3','"Dual Wield"+2','Crit. hit damage +1%',}},
+    }
+
+    sets.shark_bite = {
+        ammo="Demonry Core",
+        head="Umbani Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
+        legs="Ighwa Trousers",
+        feet="Skulk. Poulaines",
+        neck="Breeze Gorget",
+        waist="Windbuffet Belt",
+        left_ear="Ishvara Earring",
+        right_ear={ name="Moonshade Earring", augments={'Attack+4','TP Bonus +250',}},
+        left_ring="Apate Ring",
+        right_ring="Rajas Ring",
+        back={ name="Canny Cape", augments={'DEX+2','AGI+3','"Dual Wield"+2','Crit. hit damage +1%',}},
+    }
 
     sets.precast = {}
 
+    sets.precast.fc = {
+        ammo="Impatiens",
+        head="Lithelimb Cap",
+        body="Mekosu. Harness",
+        hands={ name="Plun. Armlets", augments={'Enhances "Perfect Dodge" effect',}},
+        legs="Ighwa Trousers",
+        feet="Skulk. Poulaines",
+        neck="Orunmila's Torque",
+        waist="Chaac Belt",
+        left_ear="Infused Earring",
+        right_ear="Loquac. Earring",
+        left_ring="Veneficium Ring",
+        right_ring="Prolix Ring",
+        back="Shadow Mantle",
+    }
+
     sets.midcast = {}
+
+    sets.midcast.nuke = {
+
+    }
+
+    sets.midcast.feeb = {
+
+    }
+
 end
 
 function pretarget(spell)
@@ -118,10 +187,30 @@ end
 
 
 function precast(spell)
+    print("precast spell.type is:")
+    print(spell.type)
+    
+    if spell.skill == 'Elemental Magic' or
+        spell.skill == 'Enfeebling Magic' then
+        equip(sets.precast.fc)
+    elseif spell.name == 'Aeolian Edge' then
+        equip(sets.alien_edge)
+    elseif spell.name == 'Shark Bite' or 
+        spell.type == 'WeaponSkill' then
+        equip(sets.shark_bite)
+    else
+    end
+
 end
 
 
 function midcast(spell)
+    if spell.skill == 'Elemental Magic' then
+        equip(sets.midcast.nuke)
+    elseif spell.skill == 'Enfeebling Magic' then
+        equip(sets.midcast.feeb)
+    else
+    end
 end
 
 function aftercast(spell)
@@ -138,11 +227,39 @@ end
 
 function status_change(new, old)
     print('staus_change ' .. new .. ' -> ' .. old)
-    if player.status =='Engaged' then
-        equip(sets.melee)
+    if player.status == 'Engaged' then
+        if buffactive['Sneak Attack'] then
+            equip(sets.sneak_attack)
+        elseif buffactive['Trick Attack'] then
+            equip(sets.trick_attack)
+        else
+            equip(sets.melee)
     elseif player.in_combat then
         equip(sets.dt)
     else
         equip(sets.idle)
+    end
+end
+
+function buff_change(name, gain, buff_details)
+    print('name: ' .. name)
+    print('gain: ')
+    print(gain)
+
+    if name == 'Sneak Attack' then
+        if gain then
+            equip(sets.sneak_attack)
+        else
+            equip(sets.melee)
+        end
+
+    elseif name == 'Trick Attack' then
+        if gain then
+            equip(sets.trick_attack)
+        else
+            equip(sets.melee)
+        end
+
+    else 
     end
 end
